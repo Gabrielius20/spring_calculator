ce14f7d425fefc3ebbe50ce08a245d0b41831090
Gabri <gabrielius.plo@gmail.com>
2024-03-18 10:18:00 +0200

    Added a method to print

--- src/main/java/com/spring/calculator/CalculatorApplication.java
+++ src/main/java/com/spring/calculator/CalculatorApplication.java
@@ -2,14 +2,30 @@ package com.spring.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+
+// Web controller lets to use RequestMapping
+// @RestController tells that String type has to be printed exactly as it is
+@RestController
+// @SpringBootApplication annotation is equals to @Configuration, @EnableAutoConfiguration, @ComponentScan.
+//This annotation is used in the class with the Main method
 @SpringBootApplication
 public class CalculatorApplication {
 
 	//
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-		System.out.println("Hello World!");
 	}
+
+	@GetMapping ("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+
 
 }
