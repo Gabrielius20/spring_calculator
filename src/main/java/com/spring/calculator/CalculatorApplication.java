daddd65889cdff76588858f2f0232871bbedc081
Gabri <gabrielius.plo@gmail.com>
2024-03-18 09:42:09 +0200

    Added a print line "Hello World!"

--- src/main/java/com/spring/calculator/CalculatorApplication.java
+++ src/main/java/com/spring/calculator/CalculatorApplication.java
@@ -9,7 +9,7 @@ public class CalculatorApplication {
 	//
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 
 }
