06584a4d8a58413a01f7b41706cf5a889dd5c3fd
Gabri <gabrielius.plo@gmail.com>
2024-03-21 14:50:10 +0200

    Added functionality

--- src/main/java/com/spring/calculator/CalculatorApplication.java
+++ src/main/java/com/spring/calculator/CalculatorApplication.java
@@ -2,8 +2,6 @@ package com.spring.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -16,15 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class CalculatorApplication {
 
-	//
 	public static void main(String[] args) {
+		// Spring will run the application that is starting the tomcat server
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
-	@GetMapping ("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
-		return String.format("Hello %s!", name);
-	}
 
 
 
--- src/main/java/com/spring/calculator/CalculatorController.java
+++ src/main/java/com/spring/calculator/CalculatorController.java
@@ -1,31 +1,69 @@
 package com.spring.calculator;
 
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.HashMap;
 
 
 // Web Controller that lets use @RequestMapping
 // It combines what the user inputs with our available resources
 // @RestController annotation lets for example: String type result needs to be printed exactly as is.
-@RestController
+// However, we need to get a view, so we use @Controller
 
 // Defines the configuration component. Inside lets create bean from methods with @Beans annotation.
 // This class level annotation lets Spring "guess"" the configuration
 // Based on dependencies (.jar library's) witch the programmer puts in the project (pom.xml)
 // In this case it works together with the main method
-
-@EnableAutoConfiguration
+@Controller
 public class CalculatorController {
     @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String hello  () {
-        return "Internet calculator<p>"
-                + "Operations: <br>"
-                + "&nbsp;&nbsp; " + "Add<br>"
-                + "&nbsp;&nbsp; " + "Subtract<br>"
-                + "&nbsp;&nbsp; " + "Multiply<br>"
-                + "&nbsp;&nbsp; " + "Divide<br>";
+    public String index() {
+        // Return the jsp file must be in webapp -> WEB-INF -> jsp
+        return "calculator.jsp";
     }
+
+
+    // Because the calculator form uses POST method we need to use POST here as well
+    @RequestMapping(method = RequestMethod.POST, value = "/calculate")
+    public String calculate(@RequestParam HashMap<String, String> numbers, ModelMap modelMap) {
+        int num1 = Integer.parseInt(numbers.get("num1"));
+        int num2 = Integer.parseInt(numbers.get("num2"));
+        String symbol = numbers.get("symbol");
+        int result = 0;
+
+        // Perform calculation based on the selected symbol
+        switch (symbol) {
+            case "+" -> result = num1 + num2;
+            case "-" -> result = num1 - num2;
+            case "*" -> result = num1 * num2;
+            case "/" -> {
+                // Check if num2 is not zero to avoid division by zero
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    // Handle division by zero error
+                    modelMap.put("error", "Can't divide by zero");
+                    return "calculator.jsp";
+                }
+            }
+            default -> {
+                return "calculator.jsp";
+            }
+        }
+
+        // Model map is used to send values from Spring MVC controller to JSP
+        modelMap.put("num1", num1);
+        modelMap.put("num2", num2);
+        modelMap.put("symbol", symbol);
+        modelMap.put("result", result);
+
+        return "calculate.jsp";
+    }
+
 }
